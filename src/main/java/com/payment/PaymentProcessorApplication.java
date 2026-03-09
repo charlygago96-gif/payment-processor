b206b49a2f69499524a1a64574a2a931d168042d
Carlos Hernandez <charlygago96@gmail.com>
2026-03-09 16:02:21 +0100

    terminado

--- src/main/java/com/payment/PaymentProcessorApplication.java
+++ src/main/java/com/payment/PaymentProcessorApplication.java
@@ -1,4 +1,4 @@
-package com.payment.payment_processor;
+package com.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class PaymentProcessorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentProcessorApplication.class, args);
 	}
-
 }
